7f79b80f4da5895809634744bd0fc1a21fd4c46d
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-03-05 23:46:37 +0200

    log

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-const contactsPath = path.join(__dirname, "/db/contacts.json");
+const contactsPath = path.join(__dirname, "/db", "contacts.json");
 
 function listContacts() {
   fs.readFile(contactsPath, "utf-8")
